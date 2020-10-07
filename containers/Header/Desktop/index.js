0e4ea04200aa82b927df26b9ac96c0f186900dc7
Jason Acido <jasonacido@gmail.com>
2020-10-07 17:46:35 +0800

    Add dark theme toggle

--- containers/Header/Desktop/index.js
+++ containers/Header/Desktop/index.js
@@ -1,12 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import { makeStyles } from "@material-ui/core/styles";
+import { grey } from "@material-ui/core/colors";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Popover from "@material-ui/core/Popover";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
 import OutdoorGrillIcon from "@material-ui/icons/OutdoorGrill";
 import TodayIcon from "@material-ui/icons/Today";
 import PopupState, { bindTrigger, bindPopover } from "material-ui-popup-state";
@@ -15,10 +20,25 @@ import { Container } from "../styles";
 
 import { APP_NAME, NAVIGATION_ITEMS } from "../constants";
 
+const toggleStyles = makeStyles(() => ({
+  switchBase: {
+    color: grey[200],
+    "&$checked": {
+      color: grey[400]
+    },
+    "&$checked + $track": {
+      backgroundColor: grey[400]
+    }
+  },
+  checked: {},
+  track: {}
+}));
+
 /**
  * Header for desktop
  */
-function DesktopHeader() {
+function DesktopHeader({ isDark, setPaletteType }) {
+  const toggleClasses = toggleStyles();
   const router = useRouter();
   const isSelected = (href) => router.asPath === href;
 
@@ -27,22 +47,23 @@ function DesktopHeader() {
     Plan: <TodayIcon />
   };
 
+  /**
+   * Handles toggling of dark mode
+   * @param {boolean} dark - Dark mode state
+   */
+  function handleToggle(dark) {
+    return function () {
+      const newType = dark ? "dark" : "light";
+      setPaletteType(newType);
+    };
+  }
+
   return (
     <>
       <Typography variant="h6" noWrap>
         {APP_NAME}
       </Typography>
       <Container>
-        {/* {NAVIGATION_ITEMS.map(({ primary, href, divider }) => (
-          <>
-            <Link key={primary} href={href}>
-              <Button variant={isSelected(href) ? "contained" : "text"} color="primary">
-                <Typography variant={isSelected(href) ? "body1" : "body2"}>{primary}</Typography>
-              </Button>
-            </Link>
-            {divider && <Divider orientation="vertical" variant="middle" flexItem />}
-          </>
-        ))} */}
         {NAVIGATION_ITEMS.map(({ title, items }) => (
           <PopupState key={title} variant="popover" popupId={title}>
             {(popupState) => (
@@ -85,8 +106,18 @@ function DesktopHeader() {
           </PopupState>
         ))}
       </Container>
+      <FormControlLabel
+        control={<Switch classes={toggleClasses} checked={isDark} onChange={handleToggle(!isDark)} name="isDark" />}
+        label="Dark mode"
+        labelPlacement="start"
+      />
     </>
   );
 }
+
+DesktopHeader.propTypes = {
+  isDark: PropTypes.bool.isRequired,
+  setPaletteType: PropTypes.func.isRequired
+};
 
 export default DesktopHeader;
