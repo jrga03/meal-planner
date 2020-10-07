c97dfcb5dace23e58839e75c24b4e70cab0e95c6
Jason Acido <jasonacido@gmail.com>
2020-10-07 17:46:11 +0800

    Fix linting errors

--- containers/Recipes/styles.js
+++ containers/Recipes/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Wrapper = styled.main`
   height: 100%;
@@ -7,4 +7,4 @@ export const Wrapper = styled.main`
   flex-direction: column;
   align-items: center;
   overflow-y: auto;
-`
+`;
