b5b1fbcfa7c3009702068cf116386e71ebca0bdb
Jason Acido <jasonacido@gmail.com>
2020-10-10 23:10:33 +0800

    Add usePreventRouteChangeIf

--- utils/hooks.js
+++ utils/hooks.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 /**
  * Hook for getting window size
@@ -61,3 +62,35 @@ export function useDebounce(value, delay = 500) {
 
   return debouncedValue;
 }
+
+/**
+ * Hook that listens to `next/router` `'routeChangeStart'` events and prevents changing
+ * to a requested URL when `shouldPreventRouteChange` is `true`.
+ *
+ * @param {boolean} shouldPreventRouteChange Whether to prevent all Next.js route changes or not.
+ * @param {Function} [onRouteChangePrevented] Callback function called when route change was prevented (optional).
+ */
+export function usePreventRouteChangeIf(shouldPreventRouteChange, onRouteChangePrevented) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const routeChangeStart = (url) => {
+      if (router.asPath !== url && shouldPreventRouteChange) {
+        router.events.emit("routeChangeError");
+        onRouteChangePrevented && onRouteChangePrevented(url);
+        // Following is a hack-ish solution to abort a Next.js route change
+        // as there's currently no official API to do so
+        // See https://github.com/zeit/next.js/issues/2476#issuecomment-573460710
+        throw `Route change to "${url}" was aborted (this error can be safely ignored). See https://github.com/zeit/next.js/issues/2476.`;
+      }
+    };
+
+    router.events.on("routeChangeStart", routeChangeStart);
+
+    return () => {
+      router.events.off("routeChangeStart", routeChangeStart);
+    };
+  }, [onRouteChangePrevented, router.asPath, router.events, shouldPreventRouteChange]);
+}
+
+export default usePreventRouteChangeIf;
