ecb8d928e73f6e18a7f1aaa355056c2c3a6cde1f
Jason Acido <jasonacido@gmail.com>
2020-10-19 08:47:41 +0800

    Add API route to save recipe

---
+++ pages/api/recipe/save.js
@@ -0,0 +1,27 @@
+import { connectToDatabase } from "utils/mongodb";
+
+export default async function saveRecipe(req, res) {
+  try {
+    if (!req.body) {
+      res.status(406).end("Request body required");
+      return;
+    }
+
+    const data = JSON.parse(req.body);
+    if (!data || typeof data !== "object") {
+      res.status(406).end("Request body must be in JSON format");
+      return;
+    }
+
+    const { db } = await connectToDatabase();
+    await db
+      .collection("recipes")
+      .insertOne(data)
+      .then((result) => {
+        res.status(200).json({ id: result.insertedId });
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(error.status || 500).end(error.message);
+  }
+}
