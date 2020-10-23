2f0c385b79b0850e7d9cd8db857061fa336e58a2
Jason Acido <jasonacido@gmail.com>
2020-10-23 17:14:54 +0800

    Setup home redirect

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
+/* eslint-disable no-undef */
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
   pwa: {
     dest: "public",
     disable: process.env.NODE_ENV === "development"
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/recipes",
+        permanent: true
+      }
+    ];
   }
 });
