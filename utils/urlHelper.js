3073154231daf7fecea8aefd20a1fc49ccee9116
Jason Acido <jasonacido@gmail.com>
2020-10-24 08:23:30 +0800

    Add createAddRecipeUrl helper function

--- utils/urlHelper.js
+++ utils/urlHelper.js
@@ -1,6 +1,13 @@
-export default function createLoginUrl(redirectTo) {
+export function createLoginUrl(redirectTo) {
   if (redirectTo) {
     return `/api/auth/login?redirectTo=${encodeURIComponent(redirectTo)}`;
   }
   return `/api/auth/login`;
+}
+
+export function createAddRecipeUrl(url) {
+  if (url) {
+    return `/recipe/add?import=${encodeURIComponent(url)}`;
+  }
+  return `/recipe/add`;
 }
